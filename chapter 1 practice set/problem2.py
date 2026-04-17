0301fd39ed957216974a5dd64476414789a2ac7e
Himanshu Sankhla <himanshu.devwork@gmail.com>
2026-04-17 19:16:40 +0530

    Delete chapter 1 practice set directory

--- chapter 1 practice set/problem2.py
+++
@@ -1,4 +0,0 @@
-import pyttsx3
-pyttsx3.speak('''Twinkle, twinkle, little star, how I wonder what you are. Up above the world so high,
-like a diamond in the sky. Twinkle, twinkle, little star, how I wonder what you are.''')
-
